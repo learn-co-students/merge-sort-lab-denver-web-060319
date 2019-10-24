0004af87cc063619020fb5b7c1d45be2c80b72ce
“Patrick <pctennislessons@gmail.com>
2019-10-24 15:46:16 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function findMinAndRemoveSorted(array) {
+ return array.shift()
+}
+
+function merge(array1, array2) {
+    const mergedArray = []
+    while(array1.length != 0 && array2.length != 0){
+        if(array1[0] < array2[0]){
+            mergedArray.push(findMinAndRemoveSorted(array1))
+        }else{
+            mergedArray.push(findMinAndRemoveSorted(array2))
+        }
+    }
+    return mergedArray.concat(array1).concat(array2)
+}
+
+function mergeSort(array){
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    let sorted;
+  
+    if(array.length < 2){
+      return array
+    } else {
+      sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return sorted
+  }
 
